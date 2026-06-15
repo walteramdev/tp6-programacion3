b7eb79e1cb8d51e67f423a69e238617fbe5ee369
Yessica Esmeralda <144051031+yessica-22@users.noreply.github.com>
2026-06-15 17:20:45 -0300

    create project

---
+++ tp6-programacion3/src/test/java/ar/com/unlar/edu/tp6_programacion3/Tp6Programacion3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.unlar.edu.tp6_programacion3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp6Programacion3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
